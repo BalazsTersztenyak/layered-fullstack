9d74fdeb2331ebae29cd93eec6c0a141d0947c58
Balu <Balu@DESKTOP-1D6VN6E>
2022-03-20 21:45:07 +0100

    Add project files.

---
+++ T1ELF0_HFT_2021222.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace T1ELF0_HFT_2021222.Client
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ T1ELF0_HFT_2021222.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace T1ELF0_HFT_2021222.Models
+{
+	public class Class1
+	{
+	}
+}
