93073640c9313bfaeef30b7960e4f15ff9efc29f
Balu <Balu@DESKTOP-1D6VN6E>
2022-05-04 01:13:34 +0200

    rentalLogic create exception

--- T1ELF0_HFT_2021222.Logic/RentalLogic.cs
+++ T1ELF0_HFT_2021222.Logic/RentalLogic.cs
@@ -23,6 +23,11 @@ namespace T1ELF0_HFT_2021222.Logic
 
 		public void Create(Rental item)
 		{
+			if (Read(item.Id) != null)
+			{
+				throw new Exception("Id already in use");
+			}
+
 			this.repo.Create(item);
 		}
 
