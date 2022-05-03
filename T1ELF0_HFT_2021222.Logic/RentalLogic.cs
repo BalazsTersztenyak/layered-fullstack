0f9e84f116ec1ebc81bd7e0cc45983841037371e
Balu <Balu@DESKTOP-1D6VN6E>
2022-05-03 23:00:44 +0200

    rental logic crud

---
+++ T1ELF0_HFT_2021222.Logic/RentalLogic.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using T1ELF0_HFT_2021222.Models;
+using T1ELF0_HFT_2021222.Repository;
+
+namespace T1ELF0_HFT_2021222.Logic
+{
+	public class RentalLogic
+	{
+		IRepository<Rental> repo;
+
+		public RentalLogic(IRepository<Rental> repo)
+		{
+			this.repo = repo;
+		}
+
+		public void Create(Rental item)
+		{
+			this.repo.Create(item);
+		}
+
+		public void Delete(int id)
+		{
+			if (this.repo.Read(id) == null)
+			{
+				throw new Exception("Item not found");
+			}
+
+			this.repo.Delete(id);
+		}
+
+		public Rental Read(int id)
+		{
+			if (this.repo.Read(id) == null)
+			{
+				throw new Exception("Item not found");
+			}
+
+			return this.repo.Read(id);
+		}
+
+		public IQueryable<Rental> ReadAll()
+		{
+			return this.repo.ReadAll();
+		}
+
+		public void Update(Rental item)
+		{
+			if (Read(item.Id) == null)
+			{
+				throw new Exception("Item not found");
+			}
+
+			this.repo.Update(item);
+		}
+	}
+}
