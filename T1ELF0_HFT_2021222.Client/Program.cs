e15b1ebe60e52020091bbc4e8ef06076d7a7415d
Balu <Balu@DESKTOP-1D6VN6E>
2022-05-04 19:53:26 +0200

    console menu

--- T1ELF0_HFT_2021222.Client/Program.cs
+++ T1ELF0_HFT_2021222.Client/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleTools;
+using System;
 using T1ELF0_HFT_2021222.Logic;
 using T1ELF0_HFT_2021222.Repository;
 
@@ -19,11 +20,79 @@ namespace T1ELF0_HFT_2021222.Client
 			BrandLogic brandLogic = new BrandLogic(brandRepo, carRepo);
 			RentalLogic rentalLogic = new RentalLogic(rentalRepo, carRepo, brandRepo);
 
-			var q = brandLogic.AVGByBrand();
-			var q2 = rentalLogic.RentCountByBrand();
-			var q3 = carLogic.CountByBrand();
-			var q4 = rentalLogic.RentedAfterMarch();
-			var q5 = rentalLogic.MostPopular();
+			//var q = brandLogic.AVGByBrand();
+			//var q2 = rentalLogic.RentCountByBrand();
+			//var q3 = carLogic.CountByBrand();
+			//var q4 = rentalLogic.RentedAfterMarch();
+			//var q5 = rentalLogic.MostPopular();
+
+			var rentalSubMenu = new ConsoleMenu(args, level: 1)
+				.Add("List", () => List("rental"))
+				.Add("Create", () => Create("rental"))
+				.Add("Read", () => Read("rental"))
+				.Add("Update", () => Update("rental"))
+				.Add("Delete", () => Delete("rental"))
+				.Add("Exit", ConsoleMenu.Close);
+
+			var brandSubMenu = new ConsoleMenu(args, level: 1)
+				.Add("List", () => List("brand"))
+				.Add("Create", () => Create("brand"))
+				.Add("Read", () => Read("brand"))
+				.Add("Update", () => Update("brand"))
+				.Add("Delete", () => Delete("brand"))
+				.Add("Exit", ConsoleMenu.Close);
+
+			var carSubMenu = new ConsoleMenu(args, level: 1)
+				.Add("List", () => List("car"))
+				.Add("Create", () => Create("car"))
+				.Add("Read", () => Read("car"))
+				.Add("Update", () => Update("car"))
+				.Add("Delete", ()=> Delete("car"))
+				.Add("Exit", ConsoleMenu.Close);
+
+			var menu = new ConsoleMenu(args, level: 0)
+				.Add("Cars", () => carSubMenu.Show())
+				.Add("Brand", () => brandSubMenu.Show())
+				.Add("Rentals", () => rentalSubMenu.Show())
+				.Add("Exit", ConsoleMenu.Close);
+
+			menu.Show();
+		}
+
+		private static void Delete(string v)
+		{
+			switch (v)
+			{
+				case "car":
+
+					break;
+				case "brand":
+					break;
+				case "rental":
+					break;
+				default:
+					break;
+			}
+		}
+
+		private static void Update(string v)
+		{
+			throw new NotImplementedException();
+		}
+
+		private static void Read(string v)
+		{
+			throw new NotImplementedException();
+		}
+
+		private static void Create(string v)
+		{
+			throw new NotImplementedException();
+		}
+
+		private static void List(string v)
+		{
+			throw new NotImplementedException();
 		}
 	}
 }
